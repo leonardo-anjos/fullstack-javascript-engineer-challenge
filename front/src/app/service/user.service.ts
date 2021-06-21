2a4439913292e667c44dc1884029be1e6a269139
Leonardo Anjos <antonioleonardoanjos@gmail.com>
2021-06-21 16:41:30 -0300

    services

---
+++ front/src/app/service/user.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import axios, { AxiosResponse } from 'axios';
+import { environment } from '../../environments/environment';
+import { IUser, UserDto } from '../interfaces/user.interfaces';
+
+@Injectable()
+export class UserService {
+  constructor() { }
+
+  getAll(): Promise<Array<IUser>> {
+    return new Promise((resolve, reject) => {
+      axios.get(`${environment.api.url}/user`)
+        .then((result: AxiosResponse) => {
+          resolve(result.data);
+        }).catch(reject);
+    });
+  }
+
+  save(user: UserDto): Promise<any> {
+    axios.defaults.timeout = 1200000;
+    return new Promise((resolve, reject) => {
+      axios.post(`${environment.api.url}/user`, user)
+        .then((result: AxiosResponse) => {
+          resolve(result.data);
+        }).catch(err => {
+          reject(err.response.data);
+        });
+    });
+  }
+
+  delete(userId: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      axios.delete(`${environment.api.url}/user/${userId}/delete`)
+        .then((result: AxiosResponse) => {
+          resolve(result.data);
+        }).catch(err => {
+          reject(err.response.data);
+        });
+    });
+  }
+
+  update(userId: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      axios.put(`${environment.api.url}/user/${userId}/delete`)
+        .then((result: AxiosResponse) => {
+          resolve(result.data);
+        }).catch(err => {
+          reject(err.response.data);
+        });
+    });
+  }
+}
