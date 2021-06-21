3e14d729c8904615bb4019662472ed0112dd321e
Leonardo Anjos <antonioleonardoanjos@gmail.com>
2021-06-21 20:08:43 -0300

    modules routes config

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,19 +1,18 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TokenInterceptor } from './core/interceptor';
+import { LoginComponent } from './screens/login/login.component';
 import { AddUserTypeComponent } from './screens/user-type/add-user-type/add-user-type.component';
 import { AddUserComponent } from './screens/user/add-user/add-user.component';
 import { EditUserComponent } from './screens/user/edit-user/edit-user.component';
 import { ListUserComponent } from './screens/user/list-user/list-user.component';
-import { LoginComponent } from './screens/login/login.component';
-import { routing } from './app.routing';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { UserService } from './service/user.service';
 import { UserTypeService } from './service/user-type.service';
-import { TokenInterceptor } from './core/interceptor';
+import { UserService } from './service/user.service';
 
 @NgModule({
   declarations: [
@@ -28,7 +27,6 @@ import { TokenInterceptor } from './core/interceptor';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    routing,
     ReactiveFormsModule,
     HttpClientModule
   ],
