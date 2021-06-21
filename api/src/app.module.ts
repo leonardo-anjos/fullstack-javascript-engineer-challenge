6c07099d87192d4b501a1ce374bf04aa5201392e
Leonardo Anjos <antonioleonardoanjos@gmail.com>
2021-06-21 14:24:49 -0300

    sqlite integration database and dump

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { UserTypeModule } from './user-type/user-type.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from './config/config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    UserTypeModule,
+    TypeOrmModule.forRoot({
+      //   // type: process.env.DB_CONNECTION,
+      //   // host: process.env.DB_HOST,
+      //   // port: process.env.DB_PORT,
+      //   // username: process.env.DB_USER,
+      //   // password: process.env.DB_PASSWORD,
+      //   // database: process.env.DB_DATABASE,
+      //   // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      //   // synchronize: true,
+      type: 'sqlite',
+      database: 'database.db',
+      synchronize: true,
+      logging: false,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+    // TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
